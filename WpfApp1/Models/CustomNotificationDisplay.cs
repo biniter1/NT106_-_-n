ec04a2d7a38667bb5e3097ec9a5948cfb53f06f1
biniter1 <hasonbin123@gmail.com>
2025-07-02 21:24:29 +0700

    fix notify

---
+++ WpfApp1/Models/CustomNotificationDisplay.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using ToastNotifications.Core;
+
+namespace WpfApp1.Models
+{
+    internal class CustomNotificationDisplay : NotificationDisplayPart
+    {
+        public CustomNotificationDisplay(FrameworkElement content, NotificationBase notification)
+        {
+            Content = content;
+            Bind(notification);
+        }
+    }
+}
